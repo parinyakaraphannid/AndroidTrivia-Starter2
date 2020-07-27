d99d1e1ec583c6cfd3f259e0466a214329a22c9a
PARINYA <parinya19948@gmail.com>
2020-07-27 18:21:09 +0700

    03.3: Start an external activity  4. Task: Add and pass arguments  Step 2: Pass the arguments

--- app/src/main/java/com/example/android/navigation/GameOverFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameOverFragment.kt
@@ -31,9 +31,11 @@ class GameOverFragment : Fragment() {
         // Inflate the layout for this fragment
         val binding: FragmentGameOverBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_game_over, container, false)
-        binding.tryAgainButton.setOnClickListener{view: View->
+        binding.tryAgainButton.setOnClickListener { view: View ->
             view.findNavController()
-                    .navigate(R.id.action_gameOverFragment_to_gameFragment)}
+                    .navigate(GameOverFragmentDirections.actionGameOverFragmentToGameFragment())
+        }
+
         return binding.root
     }
 }
--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -33,9 +33,10 @@ class GameWonFragment : Fragment() {
         // Inflate the layout for this fragment
         val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_game_won, container, false)
-        binding.nextMatchButton.setOnClickListener{view: View->
+        binding.nextMatchButton.setOnClickListener { view: View ->
             view.findNavController()
-                    .navigate(R.id.action_gameWonFragment_to_gameFragment)}
+                    .navigate(GameWonFragmentDirections.actionGameWonFragmentToGameFragment())
+        }
         val args = GameWonFragmentArgs.fromBundle(arguments!!)
         Toast.makeText(context, "NumCorrect: ${args.numCorrect}, NumQuestions: ${args.numQuestions}", Toast.LENGTH_LONG).show()
         return binding.root
